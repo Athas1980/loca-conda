a1c1b0db765526a5052703052a9acaf55fbb3869
Wesley Acheson <wesley.acheson@gmail.com>
2020-10-23 10:00:28 +0100

    adds in enemy types

--- snake.lua
+++ snake.lua
@@ -1,54 +1,3 @@
-
-
----- DELETE ----
-up,down,left,right = 1,2,3,4
-function coords(x,y)
-    return {
-      x = x, y = y,
-      plus=function(self,dir)
-        local x1, y1 = x,y
-        if(dir==up) y1-=1
-        if (dir==down) y1+=1
-        if (dir==left) x1-=1
-        if (dir==right) x1+=1
-        return coords(x1, y1)
-       end
-    }
-end
-
-levels = {
-	{
-		d1 = 10,
-		delay = 6,
-		es = {6}
-	},
-	{
-		d2 =10,
-		d4 = 5,
-		acid = 5,
-		delay = 5
-	},
-	{
-		d4 = 5,
-		d5 = 5,
-		acid = 8,
-		delay = 5,
-	},{
-		d2 = 30,
-		d10 = 2,
-		l=2
-	},
-	{ d1=10, d2=5, d4=5, d5=5, d10=3, acid=10},
-	{ d1=20, d10=5, delay=3, l=2},
-	{ d4 =5, d1= 10, es={20}}
-}
-
-level = {}
-level.num = 1
------
-
-
-
 function make_snake(n)
     -- enemy snake starting locations
     local xcords = {23,18,11,6}
@@ -57,41 +6,54 @@ function make_snake(n)
     s.tail = coords(xcords[n], 1)
 	s.head = coords(xcords[n], 2)
 	s.drawn=2
+	s.hist={down}
 	s.dir=down
-	s.len = levels[level.num].es[n]
+	s.num = n
+	if (n > 0) then
+		s.len = levels[level.num].es[n]
+	else 
+		s.len=0
+	end
     s.hist = {down}
     -- the next cell this will move to
     s.next = function(self)
-        return self.head.plus(self.dir)
+        return self.head:plus(self.dir)
     end
     s.move = function(self, dir)
 		local affected = {}
 		local old = self.hist[#self.hist]
+		self.dir = dir
         self.drawn += 1
         while self.drawn > self.len do
 			add(affected, {self.tail,0})
-			self.tail=self.tail:plus(self.hist[2])
+			self.tail=self.tail:plus(self.hist[1])
 			deli(self.hist, 1)
 			self.drawn-=1 
 		end
-		printh("affected len " .. #affected)
+		add(affected, {self.tail, self:tail_sprite()})
+		add(self.hist, dir)
 		add(affected, {self.head, self:get_body_sprite(old, dir)})
-		printh("affected len " .. #affected)
 		self.head = self.head:plus(dir)
 		add(affected, {self.head, self:head_sprite()})
 		return affected
-        
+	end
+	s.blank = function(self)
+		local current = self.tail
+		local removed = {{current,0}}
+		for i=1, #self.hist do
+			current = current:plus(self.hist[i])
+			add(removed, {current,0})
+		end
+		return removed
 	end
 	s.get_body_sprite = function(self, old_dir, new_dir)
-
 		local offset = 0
 		
 		if self.enemy then 
 			offset = 4 
 		end
-		local bodyspr= 49+new_dir*16+offset
+		local bodyspr= 65+new_dir*16+offset
 		if(old_dir == new_dir) return bodyspr
-	
 		local lookup={}
 		lookup[up]={}
 		lookup[down]={}
@@ -106,7 +68,7 @@ function make_snake(n)
 		lookup[right][up] = 99
 		lookup[right][down] = 83
 	
-		return lookup[old_dir][new_dir]
+		return lookup[old_dir][new_dir]+offset
 	
 	end
 	s.head_sprite = function (self) 
@@ -115,14 +77,21 @@ function make_snake(n)
 		if self.enemy then 
 			offset = 4 
 		end
-		return 66 + (self.dir-1)*16
+		return 66 + offset + (self.dir)*16
 	end
-    return s
+	s.tail_sprite = function(self)
+		local offset = 0 if (self.enemy) offset=4
+		return 64 + offset + (self.hist[1])*16
+	end
+	return s
 end
 
 function make_player() 
     local p = make_snake(0)
-    return reset_player(p)
+	p= reset_player(p)
+	p.score =0
+	p.lives =5
+	return p
 end
 
 function reset_player(p)
